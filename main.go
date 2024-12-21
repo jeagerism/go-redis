47cbff0feca48e4308947b98f65aa51d64af4772
jeagerism <oo4dxuser@gmail.com>
2024-12-21 19:40:29 +0700

    1st

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+	app.Get("hello", func(c *fiber.Ctx) error {
+		// time.Sleep(time.Millisecond * 10)
+		return c.SendString("Hello World")
+	})
+	app.Listen(":8080")
+}
